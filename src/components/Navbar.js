9ed4dbaf4d70ffc1e4d1f22bf4c46312edb2e464
Srikanth Bisai <94598179+srikanthbisai@users.noreply.github.com>
2024-11-06 20:16:22 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { SubmitButton } from '../submit'; // Ensure this path is correct
+import { SubmitButton } from '../submit'; 
 
 function Navbar() {
   return (
     <div style={{ display: 'flex', justifyContent: 'space-between', padding:"0 40px", alignItems: 'center',background:"black", color:"white", fontFamily:"serif", letterSpacing:"1px"
      }}>
-      <h1>React Flow</h1> {/* Add any branding or title here */}
-      <SubmitButton /> {/* This will render the submit button */}
+      <h1>React Flow</h1> 
+      <SubmitButton /> 
     </div>
   );
 }
